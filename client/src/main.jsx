7d6abb4f2568e6956fdce28117a4a04dd6ab5602
Basthatan a.k.a Baby Jesus a.k.a Bass <panjiahmadbasthatanfialillmi@gmail.com>
2025-12-01 23:02:25 +0700

    Update API URL ke Railway

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import axios from 'axios'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+axios.defaults.baseURL = 'https://katalogin-apps-production.up.railway.app';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
